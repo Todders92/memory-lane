5cb1d8d16a062ead3d4416164f88c2c87aa1f948
Todd Walraven <cod4247@yahoo.com>
2020-05-04 11:47:26 -0700

    create app, add initial file structure
    Co-authored-by: Matt Taylor <taylor.matt@protonmail.com>
    Co-authored-by: Alex Skreen <skreenalex@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "./Header";
+import MemoryControl from "./MemoryControl";
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <MemoryControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
